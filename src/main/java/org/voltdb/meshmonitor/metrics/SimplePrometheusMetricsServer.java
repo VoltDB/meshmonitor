f067d78b04897503ab2e4c4998a59cceb72df5c1
Tomasz Kowalczewski <tkowalczewski@voltdb.com>
2024-02-22 13:31:11 +0100

    ENG-25319: Enhanced meshmonitor output.

--- src/main/java/org/voltdb/meshmonitor/metrics/SimplePrometheusMetricsServer.java
+++ src/main/java/org/voltdb/meshmonitor/metrics/SimplePrometheusMetricsServer.java
@@ -1,29 +1,25 @@
 package org.voltdb.meshmonitor.metrics;
 
+import com.sun.net.httpserver.HttpServer;
+import org.voltdb.meshmonitor.ConsoleLogger;
+import org.voltdb.meshmonitor.ServerManager;
+
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 
-import org.voltdb.meshmonitor.ConsoleLogger;
-import org.voltdb.meshmonitor.ServerManager;
-
-import com.sun.net.httpserver.HttpServer;
-
 public class SimplePrometheusMetricsServer {
 
     private final ConsoleLogger logger;
-    private final String hostName;
     private final InetSocketAddress bindAddress;
     private final ServerManager serverManager;
     private HttpServer server;
 
     public SimplePrometheusMetricsServer(
             ConsoleLogger logger,
-            String hostName,
             InetSocketAddress bindAddress,
             ServerManager serverManager) {
         this.logger = logger;
-        this.hostName = hostName;
         this.bindAddress = bindAddress;
         this.serverManager = serverManager;
     }
@@ -33,7 +29,7 @@ public class SimplePrometheusMetricsServer {
             server = HttpServer.create(bindAddress, 100);
             server.createContext(
                     "/metrics",
-                    new MetricsHttpHandler(hostName, serverManager));
+                    new MetricsHttpHandler(bindAddress.getHostName(), serverManager));
             server.start();
 
             logger.log("HTTP server listening on http://%s/metrics", server.getAddress());
