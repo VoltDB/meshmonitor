1ccaa7064df48e993484da2bec0dbfbf04d1dc0f
Tomasz Kowalczewski <tkowalczewski@voltdb.com>
2024-03-18 11:32:02 +0100

    Added tests for cli command errors and monitor. (#7)

--- src/test/java/org/voltdb/meshmonitor/MonitorTest.java
+++ src/test/java/org/voltdb/meshmonitor/MonitorTest.java
@@ -12,11 +12,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.time.Duration;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.List;
+import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -25,7 +28,7 @@ import static org.mockito.Mockito.*;
 
 class MonitorTest {
 
-    private static final InetSocketAddress REMOTE_ID_1 = new InetSocketAddress("10.1.0.2", 8080);
+    private static final InetSocketAddress REMOTE_ID_1 = address("10.1.0.2", 8080);
 
     @Test
     void shouldInitialiseState() {
@@ -87,7 +90,7 @@ class MonitorTest {
         ConsoleLogger logger = ConsoleLoggerTest.loggerForTest();
         MeshMonitorTimings timings = MeshMonitorTimings.createDefault(logger);
 
-        InetSocketAddress socketAddress = new InetSocketAddress("10.1.0.2", ThreadLocalRandom.current().nextInt() & 0xFFFF);
+        InetSocketAddress socketAddress = address("10.1.0.2");
         Socket socket = mock(Socket.class);
         when(socket.getRemoteSocketAddress()).thenReturn(socketAddress);
 
@@ -130,5 +133,121 @@ class MonitorTest {
 
             verify(meshMonitor).onDisconnect(eq(socketAddress), any());
         });
+    }
+
+    @Test
+    void shouldSendAndReceivePing() throws IOException, ExecutionException, InterruptedException, TimeoutException {
+        // Given
+        ConsoleLogger logger = ConsoleLoggerTest.loggerForTest();
+        MeshMonitorTimings timings = MeshMonitorTimings.createDefault(logger);
+
+        InetSocketAddress nodeA = address("127.0.0.1");
+        InetSocketAddress nodeB = address("127.0.0.1");
+
+        ServerSocketChannel nodeBChannel = ServerSocketChannel.open();
+        nodeBChannel.socket().bind(nodeB);
+        Future<SocketChannel> nodeAConnection = Executors.newFixedThreadPool(1).submit(nodeBChannel::accept);
+
+        SocketChannel connectionToNodeB = SocketChannel.open(nodeB);
+
+        MeshMonitor meshMonitor1 = mock(MeshMonitor.class);
+        MeshMonitor meshMonitor2 = mock(MeshMonitor.class);
+
+        List<InetSocketAddress> ipList = List.of(
+                address("10.1.0.2", 42),
+                address("10.1.0.3", 42),
+                address("10.1.0.4", 42)
+        );
+
+        when(meshMonitor1.getConnections()).thenReturn(ipList);
+        when(meshMonitor2.getConnections()).thenReturn(ipList);
+
+        // When
+        Monitor monitor1 = new Monitor(
+                logger,
+                meshMonitor1,
+                timings,
+                Duration.ofMillis(5),
+                connectionToNodeB,
+                nodeB
+        );
+        monitor1.start();
+
+        Monitor monitor2 = new Monitor(
+                logger,
+                meshMonitor2,
+                timings,
+                Duration.ofMillis(5),
+                nodeAConnection.get(5, TimeUnit.SECONDS),
+                nodeA
+        );
+        monitor2.start();
+
+        // Then
+        await().atMost(Durations.TEN_SECONDS).untilAsserted(() -> {
+            verify(meshMonitor1, atLeast(1)).onNewNodeInMesh(nodeB, ipList);
+            verify(meshMonitor2, atLeast(1)).onNewNodeInMesh(nodeA, ipList);
+        });
+    }
+
+    @Test
+    void shouldReportDisconnect() throws IOException, ExecutionException, InterruptedException, TimeoutException {
+        // Given
+        ConsoleLogger logger = ConsoleLoggerTest.loggerForTest();
+        MeshMonitorTimings timings = MeshMonitorTimings.createDefault(logger);
+
+        InetSocketAddress nodeB = address("127.0.0.1");
+
+        ServerSocketChannel nodeBChannel = ServerSocketChannel.open();
+        nodeBChannel.socket().bind(nodeB);
+        Future<SocketChannel> nodeAConnection = Executors.newFixedThreadPool(1).submit(nodeBChannel::accept);
+
+        SocketChannel connectionToNodeB = SocketChannel.open(nodeB);
+
+        MeshMonitor meshMonitor1 = mock(MeshMonitor.class);
+        MeshMonitor meshMonitor2 = mock(MeshMonitor.class);
+
+        List<InetSocketAddress> ipList = List.of(
+                address("10.1.0.2", 42),
+                address("10.1.0.3", 42),
+                address("10.1.0.4", 42)
+        );
+
+        when(meshMonitor1.getConnections()).thenReturn(ipList);
+        when(meshMonitor2.getConnections()).thenReturn(ipList);
+
+        // When
+        Monitor monitor1 = new Monitor(
+                logger,
+                meshMonitor1,
+                timings,
+                Duration.ofMillis(5),
+                connectionToNodeB,
+                nodeB
+        );
+        monitor1.start();
+
+        nodeAConnection.get(5, TimeUnit.SECONDS);
+        nodeBChannel.close();
+        connectionToNodeB.close();
+
+        // Then
+        await().atMost(Durations.TEN_MINUTES).until(() -> !monitor1.isRunning());
+
+        // Invoked by both sending and receiving thread
+        verify(meshMonitor1, times(2)).onDisconnect(eq(nodeB), any());
+    }
+
+    public static InetSocketAddress address(String hostname, int port) {
+        return new InetSocketAddress(hostname, port & 0xFFFF);
+    }
+
+    public static InetSocketAddress address(String hostname) {
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            int port = serverSocket.getLocalPort();
+            return address(hostname, port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
